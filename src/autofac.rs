bc156c251521fae1dce18e939a3a11ae3ac7bac5
Mcat12 <newtoncat12@yahoo.com>
2020-01-13 22:21:21 -0500

    Add mockall and test using mocked component

--- src/autofac.rs
+++ src/autofac.rs
@@ -3,14 +3,16 @@
 
 use std::sync::Arc;
 
-use shaku::Interface;
-use shaku_derive::Component;
+#[cfg(test)]
+use mockall::automock;
+use shaku::{Component, Interface};
 
 // IOutput & ConsoleOutput implementation
 // ---------------------------------------------------------------------
+#[cfg_attr(test, automock)]
 pub trait IOutput: Interface {
     fn write(&self, content: String);
-    fn get_date(&self, content: String) -> String;
+    fn format_output(&self, content: String) -> String;
 }
 
 #[derive(Component)]
@@ -28,7 +30,7 @@ impl IOutput for ConsoleOutput {
         );
     }
 
-    fn get_date(&self, content: String) -> String {
+    fn format_output(&self, content: String) -> String {
         format!("{}#{} {}", self.prefix, self.other_param, content)
     }
 }
@@ -58,6 +60,42 @@ impl IDateWriter for TodayWriter {
     fn get_date(&self) -> String {
         let mut content = "Today is ".to_string();
         content.push_str(self.today.as_str());
-        self.output.get_date(content)
+        self.output.format_output(content)
+    }
+}
+
+mod tests {
+    use shaku::{ContainerBuildContext, ContainerBuilder, Error};
+    use shaku::parameter::ParameterMap;
+
+    use crate::autofac::{IDateWriter, IOutput, MockIOutput, TodayWriter};
+
+    #[test]
+    fn date_writer_formats_correctly() {
+        // Create the mocked component data
+        let build = |build_context: &mut ContainerBuildContext,
+                     _params: &mut ParameterMap|
+         -> Result<(), Error> {
+            let mut mock = MockIOutput::new();
+            mock.expect_format_output()
+                .times(1)
+                .returning(|content| format!("PREFIX: {}", content));
+
+            build_context.insert_resolved_component::<dyn IOutput>(Box::new(mock));
+            Ok(())
+        };
+        let dependencies = Vec::new();
+
+        // Register components
+        let mut builder = ContainerBuilder::new();
+        builder.register_lambda::<dyn IOutput>("MockIOutput", Box::new(build), dependencies);
+        builder
+            .register_type::<TodayWriter>()
+            .with_named_parameter("today", "TODAY".to_owned());
+        let container = builder.build().unwrap();
+
+        // Test date writer
+        let date_writer = container.resolve_ref::<dyn IDateWriter>().unwrap();
+        assert_eq!("PREFIX: Today is TODAY", date_writer.get_date());
     }
 }
